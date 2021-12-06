f112f32f3b2d32cc6ff21e1c9be1964aeaf2a53b
Krish Sapru <ksapru@bu.edu>
2021-12-06 15:44:13 -0500

    Update AboutPage.js

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,12 +5,13 @@ import Group135 from './Assets/Group135.png';
 import { Image, Row, Container, Col } from 'react-bootstrap';
 import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
+import Navbar from './navigation';
 
 function About() {
     return (
         <div class="teff">
-      
-              <Image src={Group135} style={{align: 'center', display:'block', width:'auto%', height:'auto', maxHeight:'1260px',marginLeft:'15px', marginRight:'auto'}}rounded /> 
+                <Navbar></Navbar>
+              <Image src={Group135} style={{align: 'center', display:'block', width:'auto%', height:'auto', marginTop: '30px', maxHeight:'825px',marginLeft:'200px', marginRight:'auto'}}rounded /> 
          
         </div>
     )
