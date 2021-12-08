5d199105939c6de0d10847e95d4ae8f3e3200eb6
Krish Sapru <ksapru@bu.edu>
2021-12-08 01:55:26 -0500

    Update mapPage.js

--- src/components/mapPage.js
+++ src/components/mapPage.js
@@ -1,111 +1,129 @@
+import React, { Component } from "react";
+import {
+  Button,
+  Input,
+  Card,
+  CardBody,
+  CardImage,
+  CardTitle,
+  CardText
+} from "mdbreact";
 
-import Searchbar from './Searchbar';
 
-import Navbar from './navigation';
 
-import React, {Component, useState,useRef,useEffect} from 'react';
-import "bootstrap/dist/css/bootstrap.css";
-
+import Navbar from './Navbar'
 import SliderPage from './Slider'
 
 import DropDown_typeOfActivism from './Dropdown_typeOfActivism';
 
 import ReactMapGL, { Marker,Layer, Feature } from 'react-map-gl'
-
 import Example from './Dropdown_checkBox';
 
 import MySelect from './MySelect.js';
 
 import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import DropDown_issueType from './Dropdown_checkBox.js';
-import Card from '@mui/material/Card';
 
 import Map from './Map'
 
 import CustomMarker from './CustomMarker'
 
 import { BrowserRouter as Router } from 'react-router-dom';
-import countries from './countries.json'
 import Sidebar from './sideBar'
 import countriestwo from './countriestwo.js'
 import { render } from 'react-dom';
-// import homePage from './components/homePage';
 
-// import './components/CustomMarker'
-// import CustomMarker from './components/CustomMarker'
+import countries from "./countries.json";
+
 mapboxgl.accessToken = 'pk.eyJ1IjoidHphbGl5YSIsImEiOiJja3VuMncxd3QzeHI3MnZtbmZyOTE0Z2RhIn0.wXgglO-cXtCIq-QJ17Jv-g';
 
 
+class MapPage extends Component {
+  state = {
+    search: ""
+  };
 
-export default function MapPage(){
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-71.1029);
-    const [lat, setLat] = useState(42.3486);
-    const [zoom, setZoom] = useState(15.48);
-    const [filteredData, setFilteredData] = useState([]);
-    const [wordEntered, setWordEntered] = useState("");
-        
-    const handleFilter = (event) => {
-        const searchWord = event.target.value;
-        setWordEntered(searchWord);
-        const newFilter = countries.filter((value) => {
-            return value.title.toLowerCase().includes(searchWord.toLowerCase());
+  renderCountry = country => {
+    const { search } = this.state;
+    
+    var code = country.code.toLowerCase();
+
+    /*if( search !== "" && country.name.toLowerCase().indexOf( search.toLowerCase() ) === -1 ){
+        return null
+    }*/
+
+    return (
+      <div className="col-md-3" style={{ marginTop: "10px", marginLeft: "-10px", marginRight: "1000px", marginBottom: "20px" }}>
+        <Card>
+          <CardBody>
+            <CardTitle title={country.name}>
+              {country.name.substring(0, 16)}
+              {country.name.length > 16 && "..."}
+              <br/>
+     
+              <h6>{country.code}</h6>
+            </CardTitle>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  };
+
+  onchange = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  render() {
+    const { search } = this.state;
+    const filteredCountries = countries.filter(country => {
+      return country.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
     });
 
-    if (searchWord === "") {
-      setFilteredData([]);
-    } else {
-      setFilteredData(newFilter);
-    }
-  };
+    return (
+        
+      <div className="flyout">
+         <Navbar></Navbar>
 
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
-  };
+        <main style={{ marginTop: "4rem" }}>
  
 
 
+          <div className="container">
  
-       
-
-   // const SearchBar 
-
-    return(
-        <div className="App">
-
-        <Navbar></Navbar>
-        <div className="SearchBar">
-            <Searchbar ></Searchbar>
-            <DropDown_typeOfActivism></DropDown_typeOfActivism>
-            <DropDown_issueType></DropDown_issueType>
-            <SliderPage></SliderPage>
-        </div>
-
-        
-        
-        <div className="body">
-            <div className="sideBarContainer">
-            <Router>
-            <div className="sideBar">
-            { countries.map (post => {
-                return (
-                    <h5> {post.name} </h5>
-              
-
-                )
-            })}
+            <div className="row">
+    
+                
+              <div className="col-12">
+      
+                <center>
+                  <h3>
+            
+                  </h3>
+                </center>
+              </div>
+              <div className="col"  style={{ marginTop: "-40px", marginLeft: "-30px", marginRight: "300px" }}>
+                <Input
+                  label="Search Event"
+                  icon="search"
+                  onChange={this.onchange}
+                />
+              </div>
+              <div className="col" />
             </div>
-            </Router>
-            </div>
-            <div className="Map">
+            <div className="Map" style={{ marginTop: "-100px", marginLeft: "-70px" }}>
             <Map></Map>
             </div>
-        </div>
-        <div>
-        <homePage></homePage>
-        </div>
-    </div>
+            <div className="row">
+              {filteredCountries.map(country => {
+                return this.renderCountry(country);
+              })}
+            </div>
+          </div>
+        </main>
+        
+      </div>
     );
+  }
 }
+
+export default MapPage;
