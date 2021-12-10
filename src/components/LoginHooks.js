deb795104b94aece0e58f7f2038c219bb67a8dd0
Krish Sapru <ksapru@bu.edu>
2021-12-10 14:50:56 -0500

    Create LoginHooks.js

---
+++ src/components/LoginHooks.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useGoogleLogin } from 'react-google-login';
+
+// refresh token
+
+const clientId =
+  '698061443413-avq9q5g48vg4bp3fdq7rus8bqspa9pb9.apps.googleusercontent.com';
+
+function LoginHooks() {
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
+    );
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      `Failed to login. 😢 Please ping this to repo owner twitter.com/sivanesh_fiz`
+    );
+  };
+
+  const { signIn } = useGoogleLogin({
+    onSuccess,
+    onFailure,
+    clientId,
+    isSignedIn: true,
+    accessType: 'offline',
+    // responseType: 'code',
+    // prompt: 'consent',
+  });
+
+  return (
+    <button onClick={signIn} className="button">
+      <img src="icons/google.svg" alt="google login" className="icon"></img>
+
+      <span className="buttonText">Sign in with Google</span>
+    </button>
+  );
+}
+
+export default LoginHooks;
