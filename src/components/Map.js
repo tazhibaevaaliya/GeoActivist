0b512c03aa9438fc3621a6480b2ddfbde6d974e6
Aliya Tazhibayeva <tzaliya@bu.edu>
2022-02-17 14:46:03 -0500

    Revert "Initial Commit"
    This reverts commit 12f673ad20eaed566cd7bbbece0d9af12bd7abf5.

--- src/components/Map.js
+++ src/components/Map.js
@@ -72,24 +72,24 @@ const Map = () => {
 
 
 
-  // useEffect(() => {
-  //   async function getMyData() {
-  //     const myData = await getData();
-  //     // setMapMarkers(myData.map(item => (
-  //     //   <Marker latitude={Number(item.Latitude)} longitude={Number(item.Longtitude)} offsetLeft={-20} offsetTop={-10}>
-  //     //     <button style={{background:'none', border:'none',cursor:'pointer'}} onClick={(e)=>{
-  //     //       e.preventDefault();
-  //     //       setSelectedEvent(mapMarkers); 
-  //     //     }}>
-  //     //       <img style={{width:'20px',height:'50px'}} src={require('./Assets/mapbox-marker-icon.png').default} /> 
-  //     //       </button>
-  //     //    </Marker>)
-  //     //   ))
-  //       // console.log(mapMarkers.length)
-  //     setData(myData);
-  //   }
-  //   getMyData();
-  // }, [])
+  useEffect(() => {
+    async function getMyData() {
+      const myData = await getData();
+      // setMapMarkers(myData.map(item => (
+      //   <Marker latitude={Number(item.Latitude)} longitude={Number(item.Longtitude)} offsetLeft={-20} offsetTop={-10}>
+      //     <button style={{background:'none', border:'none',cursor:'pointer'}} onClick={(e)=>{
+      //       e.preventDefault();
+      //       setSelectedEvent(mapMarkers); 
+      //     }}>
+      //       <img style={{width:'20px',height:'50px'}} src={require('./Assets/mapbox-marker-icon.png').default} /> 
+      //       </button>
+      //    </Marker>)
+      //   ))
+        // console.log(mapMarkers.length)
+      setData(myData);
+    }
+    getMyData();
+  }, [])
 
   for (const i in data) {
     latitude.push(parseFloat(data[i].Latitude));
