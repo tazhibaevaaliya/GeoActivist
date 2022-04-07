3c7a40dd06138f42fd82bb847eb9e0dfa499f180
ScarlettLi <scartt@bu.edu>
2022-04-07 18:49:51 -0400

    scarlett's version

--- src/index.js
+++ src/index.js
@@ -17,3 +17,33 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const bodyParser = require("body-parser");
+const express = require("express");
+// const admin = require("firebase-admin");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("static"));
+
+app.get("/login", function (req, res) {
+  	res.render("login.html");
+});
+
+app.get("/signup", function (req, res) {
+  	res.render("signup.html");
+});
+
+app.get("/profile", function (req, res) {
+    res.render("profile.html");
+});
+
+app.get("/", function (req, res) {
+	res.render("signup.html");
+});
+
+app.listen(PORT, () => {
+  	console.log(`Listening on http://localhost:${PORT}`);
+});
